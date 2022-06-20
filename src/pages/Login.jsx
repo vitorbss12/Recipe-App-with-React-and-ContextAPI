5180e91d509a93659a6566a8eb1d064ae7af9fd9
Vinicius Teixeira Freitas <texera009@gmail.com>
2022-06-20 16:49:23 -0300

    adiciona requisito 07 ao projeto. Insere no local storage a chave user contendo o email do usuário

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -17,6 +17,7 @@ function Login() {
     event.preventDefault();
     localStorage.setItem('mealsToken', 1);
     localStorage.setItem('cocktailsToken', 1);
+    localStorage.setItem('user', JSON.stringify({ email: userEmailInput }));
   };
 
   return (
