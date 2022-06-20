d40340eced456bbfe30316ef750fb9cf6dd6a63f
vitorbss12 <vitor_bss10@hotmail.com>
2022-06-20 20:05:29 -0300

    Requisito 19 - Criando Footer da aplicação com icones de comidas, explorar e bebidas

--- src/pages/ExploreDrinks.jsx
+++ src/pages/ExploreDrinks.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function ExploreDrinks() {
   return (
     <div>
       <Header title="Explore Drinks" />
+      <Footer />
     </div>
   );
 }
