8dcca01d39b3e040f7ec684d99dd3e620e5f6ab8
Vinicius Teixeira Freitas <texera009@gmail.com>
2022-06-20 16:57:30 -0300

    adiciona requisito 08 ao projeto. Redireciona o usuário da página de login para a pagina principal de receitas

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './pages/Login';
-//
+import Foods from './pages/Foods';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route exact path="/foods" component={ Foods } />
       </Switch>
     </BrowserRouter>
   );
