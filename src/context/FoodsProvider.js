a1389317d561427bed1ef0d18ddadbcda563da70
Vinicius Teixeira Freitas <texera009@gmail.com>
2022-06-21 19:06:44 -0300

    corrige erros de lint

--- src/context/FoodsProvider.js
+++ src/context/FoodsProvider.js
@@ -35,7 +35,7 @@ function FoodsProvider({ children }) {
           const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${inputSearch}`);
           const foodsData = await response.json();
           if (foodsData.meals) {
-          setFoodData(foodsData.meals);
+            setFoodData(foodsData.meals);
           }
         }
         break;
