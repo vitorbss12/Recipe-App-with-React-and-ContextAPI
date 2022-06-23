4aa3b5c96cd619cf4fb4d47ef51eae997f3a625a
Paula Carvalho <henriques.paulac@gmail.com>
2022-06-23 18:00:54 -0300

    Tela de Perfil

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,50 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-function Profile() {
+function Profile({ history }) {
+  const email = JSON.parse(localStorage.getItem('user'));
+
+  const loginRoute = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
   return (
     <div>
       <Header title="Profile" />
       <Footer />
+      <p data-testid="profile-email">{ email.email }</p>
+      <button
+        type="submit"
+        data-testid="profile-done-btn"
+        onClick={ () => history.push('/done-recipes') }
+      >
+        Done Recipes
+      </button>
+      <button
+        type="submit"
+        data-testid="profile-favorite-btn"
+        onClick={ () => history.push('/favorite-recipes') }
+      >
+        Favorite Recipes
+      </button>
+      <button
+        type="submit"
+        data-testid="profile-logout-btn"
+        onClick={ loginRoute }
+      >
+        Logout
+      </button>
     </div>
   );
 }
+
+Profile.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Profile;
