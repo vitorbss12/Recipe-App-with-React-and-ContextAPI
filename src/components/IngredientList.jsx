ec217deb918d70d8707977e953bfc9cdc13f5b64
vitorbss12 <vitor_bss10@hotmail.com>
2022-06-24 15:59:07 -0300

    Requisito 49 - Item riscado quando checked

--- src/components/IngredientList.jsx
+++ src/components/IngredientList.jsx
@@ -1,32 +1,23 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 import useGetIngredients from '../hooks/useGetIngredients';
 import DrinksContext from '../context/DrinksContext';
 
 function IngredientList() {
   const { currentDrink } = useContext(DrinksContext);
-  const [ingredientDone, setIngredientDone] = useState(false);
   const ingredients = useGetIngredients(currentDrink);
-  const [itemChecked, setItemChecked] = useState(0);
 
   const buttonStyle = {
     margin: '5px',
-    listStyle: 'none',
-  };
-
-  const ingredientDoneStyle = {
-    textDecorationLine: 'line-through',
-    textDecorationStyle: 'solid',
-    margin: '5px',
-    listStyle: 'none',
+    width: '100%',
   };
 
   const handleChange = ({ target }) => {
-    const checkedItem = target.parentNode;
-    console.log(ingredients.indexOf(checkedItem));
-    setIngredientDone(!ingredientDone);
+    if (target.checked) {
+      target.parentNode.style.textDecorationLine = 'line-through';
+    } else {
+      target.parentNode.style.textDecorationLine = 'none';
+    }
   };
-
-  useEffect(() => { console.log(ingredientDone); }, [ingredientDone]);
 
   return (
     <div>
@@ -35,9 +26,8 @@ function IngredientList() {
           <label
             key={ index }
             htmlFor={ `${index}-ingredient-step` }
-            style={ ingredientDone ? (
-              ingredientDoneStyle) : (buttonStyle) }
             data-testid={ `${index}-ingredient-step` }
+            style={ buttonStyle }
           >
             <input
               type="checkbox"
