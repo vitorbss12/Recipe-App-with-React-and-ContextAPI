a20ec9fdd0c3567ef93b991e7a24d2dc61c17945
vitorbss12 <vitor_bss10@hotmail.com>
2022-09-01 14:26:46 -0300

    improve: refactor Login page with Bootstrap

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,65 +1,82 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+// import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import '../styles/Login.css';
 
-function Login({ history }) {
-  // define os estados
-  const [userEmailInput, setUserEmailInput] = useState('');
-  const [userPasswordInput, setUserPasswordInput] = useState('');
+function Login() {
+  const [userEmail, setUserEmail] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+  const [isBtnEnabled, setIsBtnEnabled] = useState(false);
+  const history = useHistory();
 
-  // faz a validação do email
-  const pattern = /\S+@\S+.com/;
-  const isEmailValid = pattern.test(userEmailInput);
-  const MIN_PASSWORD_LENGTH = 6;
-  const isBtnEnabled = isEmailValid && userPasswordInput.length > MIN_PASSWORD_LENGTH;
+  const emailValidation = (email) => {
+    const pattern = /\S+@\S+.com/;
+    return pattern.test(email);
+  };
 
-  // funções:
-  const saveTokensToLocalStorage = (event) => {
+  const passwordValidation = (password) => {
+    const MIN_PASSWORD_LENGTH = 6;
+    return password.length > MIN_PASSWORD_LENGTH;
+  };
+
+  useEffect(() => {
+    const btnStatus = emailValidation(userEmail)
+      && passwordValidation(userPassword);
+
+    setIsBtnEnabled(btnStatus);
+  }, [userEmail, userPassword]);
+
+  const loginFormsSubmit = (event) => {
     event.preventDefault();
     localStorage.setItem('mealsToken', 1);
     localStorage.setItem('cocktailsToken', 1);
-    localStorage.setItem('user', JSON.stringify({ email: userEmailInput }));
+    localStorage.setItem('user', JSON.stringify({ email: userEmail }));
     history.push('/foods');
+    console.log('submit');
   };
 
   return (
-    <div>
-      <form onSubmit={ saveTokensToLocalStorage }>
-        <label htmlFor="email">
-          Email:
-          <input
-            type="email"
-            id="email"
-            placeholder="Insira seu email"
-            data-testid="email-input"
-            onChange={ ({ target }) => setUserEmailInput(target.value) }
-          />
-        </label>
-        <label htmlFor="password">
-          Senha:
-          <input
-            type="password"
-            id="password"
-            placeholder="Insira sua senha"
-            data-testid="password-input"
-            onChange={ ({ target }) => setUserPasswordInput(target.value) }
-          />
-        </label>
-        <button
-          type="submit"
-          data-testid="login-submit-btn"
-          disabled={ !isBtnEnabled }
-        >
-          Entrar
-        </button>
-      </form>
-    </div>
+    <Container fluid className="align-self-center">
+      <Row className="justify-content-center">
+        <h2 className="login-title">Let&apos;s Cook!</h2>
+      </Row>
+      <Row className="justify-content-center">
+        <h3 className="login-subtitle">An app for those who like to cook</h3>
+      </Row>
+      <Row className="justify-content-center">
+        <Form className="login-forms" onSubmit={ loginFormsSubmit }>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              onChange={ ({ target }) => setUserEmail(target.value) }
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              onChange={ ({ target }) => setUserPassword(target.value) }
+            />
+          </Form.Group>
+          <Button
+            bsPrefix="login-btn"
+            className="md-auto"
+            type="submit"
+            disabled={ !isBtnEnabled }
+          >
+            Login
+          </Button>
+        </Form>
+      </Row>
+    </Container>
   );
 }
-
-Login.propTypes = {
-  history: PropTypes.shape({
-    push: PropTypes.func.isRequired,
-  }).isRequired,
-};
 
 export default Login;
