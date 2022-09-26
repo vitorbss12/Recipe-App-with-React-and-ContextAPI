d2a0fb86a5f687e2735884cac03c772acb19a914
vitorbss12 <vitor_bss10@hotmail.com>
2022-09-26 18:52:46 -0300

    feat: Footer com botão para iniciar, continuar receitas

--- src/pages/RecipeDetails/FoodRecipeDetail.jsx
+++ src/pages/RecipeDetails/FoodRecipeDetail.jsx
@@ -1,42 +1,25 @@
 import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
-// import Button from 'react-bootstrap/Button';
-// import Navbar from 'react-bootstrap/Navbar';
 import useFetchCurrentRecipe from '../../hooks/useFetchCurrentRecipe';
-// import useGetDoneRecipe from '../../utils/getDoneRecipe';
-// import useGetInProgressRecipe from '../../utils/getInProgressRecipe';
 import Header from '../../components/Header/Header';
 import RecipeContent from '../../components/RecipeContent/RecipeContent';
+import FooterDetails from '../../components/FooterDetails/FooterDetails';
 
 function FoodRecipeDetail() {
   const location = useLocation();
-  const [foodId] = useState(parseInt(location.pathname.split('/')[2], 10));
-  // const [doneRecipe, setDoneRecipe] = useState(false);
-  // const [inProgressRecipe, setInProgressRecipe] = useState(false);
-  useFetchCurrentRecipe('foods', foodId);
-  // const history = useHistory();
+  const [recipeId] = useState(parseInt(location.pathname.split('/')[2], 10));
 
-  // setDoneRecipe(useGetDoneRecipe(foodId));
-  // setInProgressRecipe(useGetInProgressRecipe('food', foodId));
+  useFetchCurrentRecipe('foods', recipeId);
 
   return (
-    <Container fluid="xxl" className="d-flex flex-column w-100">
+    <Container
+      fluid="xxl"
+      className="d-flex flex-column justify-content-between flex-fill"
+    >
       <Header recipeDetails />
-      <RecipeContent option="foods" id={ foodId } />
-      {/* // {
-        //   !doneRecipe && (
-        //     <Navbar fixed="bottom">
-        //       <Button
-        //         type="submit"
-        //         data-testid="start-recipe-btn"
-        //         onClick={ () => history.push(`/foods/${foodId}/in-progress`) }
-        //       >
-        //         { inProgressRecipe ? 'Continue Recipe' : 'Start Recipe' }
-        //       </Button>
-        //     </Navbar>
-        //   )
-        // } */}
+      <RecipeContent option="foods" id={ recipeId } />
+      <FooterDetails option="foods" id={ recipeId } />
     </Container>
   );
 }
