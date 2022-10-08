5c0dbbe4f418ed4d1f5556f56ae2f7454a85cd82
vitorbss12 <vitor_bss10@hotmail.com>
2022-10-08 20:45:54 -0300

    chore: Removendo comentários do código

--- src/components/RecipeContent/RecipeContent.jsx
+++ src/components/RecipeContent/RecipeContent.jsx
@@ -6,7 +6,6 @@ import RecipesContext from '../../contexts/Recipes/RecipesContext';
 import RecipeDetailsTop from '../RecipeDetailsTop/RecipeDetailsTop';
 import Ingredients from '../Ingredients/Ingredients';
 import RecipeInstructions from '../RecipeInstructions/RecipeInstructions';
-// import RecipeVideo from '../RecipeVideo/RecipeVideo';
 import Recommendations from '../Recommendations/Recommendations';
 
 function RecipeContent({ option, page, id }) {
@@ -47,7 +46,6 @@ function RecipeContent({ option, page, id }) {
             ingredients={ ingredients }
           />
           <RecipeInstructions instructions={ currentRecipe.strInstructions } />
-          {/* <RecipeVideo name={ currentFood.strMeal } url={ currentFood.strYoutube } /> */}
           <Recommendations option={ option === 'foods' ? 'drinks' : 'foods' } />
         </>
       ) : (
